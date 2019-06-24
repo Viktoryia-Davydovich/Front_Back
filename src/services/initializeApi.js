238b5ee7afed104180d35fcbb0062401c11ccbde
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-24 13:31:54 +0300

    api service

---
+++ src/services/initializeApi.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("../../src/routes/users/user");
+require("../../src/middlewares/passport")(passport);
+
+module.exports = function initApi() {
+  const app = express();
+
+  app.use(passport.initialize());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use("/api/users", users);
+
+  app.get("/", function(req, res) {
+    res.send("hello");
+  });
+
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+  });
+};
