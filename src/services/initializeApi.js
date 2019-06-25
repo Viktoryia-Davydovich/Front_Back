6fa95698833e1818dede2fab87f8cf371ac40b1f
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-25 16:40:15 +0300

    cors sht

--- src/services/initializeApi.js
+++ src/services/initializeApi.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+var cors = require("cors");
 
 const users = require("../../src/routes/users/user");
 require("../../src/middlewares/passport")(passport);
 
 module.exports = function initApi() {
   const app = express();
+
+  app.use(cors());
 
   app.use(passport.initialize());
   app.use(bodyParser.urlencoded({ extended: false }));
