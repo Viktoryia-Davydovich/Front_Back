360bd046cfc9ace0cd6036e9d33848bae66eef19
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-25 17:02:17 +0300

    cors sht congiguring

--- src/services/initializeApi.js
+++ src/services/initializeApi.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+var cors = require("cors");
 
 const users = require("../../src/routes/users/user");
 require("../../src/middlewares/passport")(passport);
@@ -8,12 +9,17 @@ require("../../src/middlewares/passport")(passport);
 module.exports = function initApi() {
   const app = express();
 
-  app.options("/register", function(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
+  app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST,GET,OPTIONS,PUT,DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Accept");
+    next();
   });
+
+  app.use(cors());
 
   app.use(passport.initialize());
   app.use(bodyParser.urlencoded({ extended: false }));
