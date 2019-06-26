b483cfae52c1b11d9ccb8a3a73a04bede4bfd046
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-06-27 00:03:28 +0300

    async wrapper

--- src/routes/users/user.js
+++ src/routes/users/user.js
@@ -12,96 +12,92 @@ const async = require("async");
 
 router.post(
   "/register",
-  wrap(
-    async(function(req, res) {
-      const { errors, isValid } = validateRegisterInput(req.body);
+  wrap(async function(req, res) {
+    const { errors, isValid } = validateRegisterInput(req.body);
 
-      if (!isValid) {
-        return res.status(400).json(errors);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    await User.findOne({
+      email: req.body.email
+    }).then(user => {
+      debugger;
+      if (user) {
+        return res.status(400).json({
+          email: "Email already exists"
+        });
+      } else {
+        const newUser = new User({
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) console.error("There was an error", err);
+          else {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) console.error("There was an error", err);
+              else {
+                newUser.password = hash;
+                newUser.save().then(user => {
+                  res.json(user);
+                });
+              }
+            });
+          }
+        });
       }
-
-      User.findOne({
-        email: req.body.email
-      }).then(user => {
-        debugger;
-        if (user) {
-          return res.status(400).json({
-            email: "Email already exists"
-          });
-        } else {
-          const newUser = new User({
-            email: req.body.email,
-            password: req.body.password
-          });
-
-          bcrypt.genSalt(10, (err, salt) => {
-            if (err) console.error("There was an error", err);
-            else {
-              bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) console.error("There was an error", err);
-                else {
-                  newUser.password = hash;
-                  newUser.save().then(user => {
-                    res.json(user);
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-    })
-  )
+    });
+  })
 );
 
 router.post(
   "/login",
-  wrap(
-    async((req, res) => {
-      debugger;
-      const { errors, isValid } = validateLoginInput(req.body);
+  wrap(async (req, res) => {
+    debugger;
+    const { errors, isValid } = validateLoginInput(req.body);
 
-      if (!isValid) {
-        return res.status(400).json(errors);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    await User.findOne({ email }).then(user => {
+      if (!user) {
+        errors.email = "User not found";
+        return res.status(404).json(errors);
       }
-
-      const email = req.body.email;
-      const password = req.body.password;
-
-      User.findOne({ email }).then(user => {
-        if (!user) {
-          errors.email = "User not found";
-          return res.status(404).json(errors);
-        }
-        bcrypt.compare(password, user.password).then(isMatch => {
-          if (isMatch) {
-            const payload = {
-              id: user.id
-            };
-            jwt.sign(
-              payload,
-              "secret",
-              {
-                expiresIn: 3600
-              },
-              (err, token) => {
-                if (err) console.error("There is some error in token", err);
-                else {
-                  res.json({
-                    success: true,
-                    token: `Bearer ${token}`
-                  });
-                }
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = {
+            id: user.id
+          };
+          jwt.sign(
+            payload,
+            "secret",
+            {
+              expiresIn: 3600
+            },
+            (err, token) => {
+              if (err) console.error("There is some error in token", err);
+              else {
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`
+                });
               }
-            );
-          } else {
-            errors.password = "Incorrect Password";
-            return res.status(400).json(errors);
-          }
-        });
+            }
+          );
+        } else {
+          errors.password = "Incorrect Password";
+          return res.status(400).json(errors);
+        }
       });
-    })
-  )
+    });
+  })
 );
 
 router.get(
