6b42b3da5772bcec7e84d75983d7b4273d0a35af
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-06-26 21:24:54 +0300

    test

--- src/services/initializeApi.js
+++ src/services/initializeApi.js
@@ -34,7 +34,7 @@ module.exports = function initApi() {
   });
 
   app.get("/", cors(), function(req, res) {
-    res.send("start");
+    res.send("up and running");
   });
 
   const PORT = process.env.PORT || 5000;
