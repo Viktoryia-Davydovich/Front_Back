0b54ecf2ca7e96c3430329ff621b28f6df92c0a2
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-06-27 22:45:11 +0300

    on app terminate handler

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const initApi = require("./src/services/initializeApi");
 
 initDb();
 initApi();
+
+process.on("SIGINT" || "SIGTERM" || "SIGQUIT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose connection is closed");
+    process.exit(0);
+  });
+});
