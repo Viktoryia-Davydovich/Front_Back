16234dd0ade5ce3a2615c1dd416c62117143bcfd
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-28 08:24:31 +0300

    async await in route handlers

--- src/services/initializeApi.js
+++ src/services/initializeApi.js
@@ -9,34 +9,12 @@ require("../../src/middlewares/passport")(passport);
 module.exports = function initApi() {
   const app = express();
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+  app.use(cors());
 
   app.use(passport.initialize());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
-  /*
-  app.use(cors());
-
-
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-    );
-    next();
-  });
-*/
   app.use("/api/users", users);
 
   app.get("/", function(req, res, next) {
