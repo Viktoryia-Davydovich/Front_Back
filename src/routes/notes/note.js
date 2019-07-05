d7794bb9861a9f61c35f6e953be8938d14c8fb40
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-07-05 08:58:52 +0300

    note schema on get request

--- src/routes/notes/note.js
+++ src/routes/notes/note.js
@@ -14,10 +14,52 @@ router.post("/notes", async (request, response) => {
     });
 
     const saveNote = await newNote.save();
+    response.json(saveNote);
   } catch (err) {
     console.log("err" + err);
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
+});
+
+router.get("/notes", async (request, response) => {
+  try {
+    const notes = await Note.find({});
+
+    response.send(
+      notes.map(note => {
+        note.title, note.content, note.tag, note.createdDate, note.updatedDate;
+      })
+    );
+  } catch (error) {
+    console.log("err" + err);
+    res.status(500).send(err);
+  }
+});
+
+router.get("/notes/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    if (!id) {
+      response.status(400).send("Id is required");
+    } else {
+      theNote = await Note.findOne(id);
+      response.send(theNote);
+    }
+  } catch (error) {
+    console.log("err" + err);
+    res.status(500).send(err);
+  }
+});
+
+router.put("/notes", async (request, response) => {
+  const { id, title, content, tag } = request.body.note;
+  noteManager
+    .updateNote(id, title, content, tag)
+    .then(() => response.status(200).send())
+    .catch(error => {
+      console.log(error.message);
+      response.status(500).send(error.message);
+    });
 });
 
 router.delete("/notes/:id", (request, response) => {
@@ -32,70 +74,6 @@ router.delete("/notes/:id", (request, response) => {
       .catch(error => {
         console.log(error.message);
         response.status(500).send();
-      });
-  }
-});
-router.get("/notes/:id", (request, response) => {
-  const { id } = request.params;
-
-  if (!id) {
-    response.status(400).send("Id is required");
-  } else {
-    noteManager
-      .findNoteById(id)
-      .then(note => response.json(note))
-      .catch(error => {
-        console.log(error.message);
-        response.status(500).send();
-      });
-  }
-});
-router.get("/notes", (request, response) => {
-  const { title, tag } = request.query;
-
-  if (title) {
-    noteManager
-      .findNotesByTitle(title)
-      .then(notes => response.json(notes))
-      .catch(error => {
-        console.log(error);
-        response.status(500).send();
-      });
-  } else if (tag) {
-    noteManager
-      .findNotesByTag(tag)
-      .then(notes => response.json(notes))
-      .catch(error => {
-        console.log(error);
-        response.status(500).send();
-      });
-  } else {
-    noteManager
-      .listNotes()
-      .then(notes => response.json(notes))
-      .catch(error => {
-        console.log(error);
-        response.status(500).send();
-      });
-  }
-});
-
-router.put("/notes", (request, response) => {
-  const { id, title, content, tag } = request.body.note;
-
-  if (!id) {
-    response.status(400).send("Id is required");
-  } else if (!title) {
-    response.status(400).send("Title is required");
-  } else if (!content) {
-    response.status(400).send("Content is required");
-  } else {
-    noteManager
-      .updateNote(id, title, content, tag)
-      .then(() => response.status(200).send())
-      .catch(error => {
-        console.log(error.message);
-        response.status(500).send(error.message);
       });
   }
 });
