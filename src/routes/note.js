17a39220ad916bcdd4c53c9f7ee143690b3f188f
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-07-07 16:26:28 +0300

    router responses into json

--- src/routes/note.js
+++ src/routes/note.js
@@ -49,7 +49,7 @@ router.get("/", async (request, response) => {
 router.get("/:id", async (request, response) => {
   try {
     const { id } = request.params;
-    theNote = await Note.findOne(id);
+    theNote = await Note.findById(id);
     if (theNote) {
       response.status(204).send("Note found");
     }
